85df1b1044b6be4b899e6a029ee4003a6e78f291
Svily_0 <980047129@qq.com>
2025-04-06 14:36:00 +0800

    fix: change port

--- proxy/Proxy.bat
+++ proxy/Proxy.bat
@@ -1,2 +1,2 @@
-git config --global http.proxy 127.0.0.1:22307
-git config --global https.proxy 127.0.0.1:22307
+git config --global http.proxy 127.0.0.1:33210
+git config --global https.proxy 127.0.0.1:33210
